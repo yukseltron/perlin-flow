02c6a1b93aee80d54f545a7687a12416a83b6788
yukseltron <h.yuksel04@gmail.com>
2022-08-08 16:02:13 -0400

    add dependecies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import PerlinFlow from './PerlinFlow';
+
+const center = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div style={center}>
+            <h1>#001</h1>
+            <h2>Perlin Flow</h2>
+        </div>
+        <PerlinFlow/>
     </div>
   );
 }
---
+++ src/Particle.js
@@ -0,0 +1,61 @@
+
+function Particle() {
+  this.pos = createVector(random(width), random(height));
+  this.vel = createVector(random(width), random(height));
+  this.acc = createVector(random(width), random(height));
+  this.maxspeed = 2;
+
+  this.prevPos = this.pos;
+
+  this.follow = function(vectors) {
+    let x = floor(this.pos.x / scl);
+    let y = floor(this.pos.y / scl);
+    let index = x + y * cols;
+    let force = vectors[index];
+    this.applyForce(force);
+
+  }
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+
+  this.show = function() {
+    stroke(0, 0, 0);
+    strokeWeight(0.5);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    line(this.pos.y, this.pos.x, this.prevPos.y, this.prevPos.x);
+    //point(this.pos.x, this.pos.y);
+    this.updatePrev();
+  }
+
+  this.updatePrev = function() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+  }
+  this.edges = function() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+  }
+}
